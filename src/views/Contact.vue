4b4339d75362780e3067b1e363e06115e2431b97
Anders Nielsen <arnielsen.acc@outlook.dk>
2019-05-17 15:16:36 +0200

    test netlify form

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -5,7 +5,40 @@
       <v-layout justify-center align-center wrap="true">
         <v-flex xs12 mb-5>
           <v-container grid-list-x1 pa-0>
-            Contact TODO
+            
+          <v-form name="contact" method="POST" data-netlify="true">
+            <v-container>
+              <v-layout row wrap>
+
+                <v-flex xs12>
+                  <v-text-field
+                    v-model="name"
+                    :rules="nameRules"
+                    label="Navn"
+                  ></v-text-field>
+                </v-flex>
+                <v-flex xs12>
+                  <v-text-field
+                    v-model="email"
+                    :rules="emailRules"
+                    label="E-mail"
+                  ></v-text-field>
+                </v-flex>
+                <v-flex xs12>
+                  <v-textarea
+                    v-model="message"
+                    :rules="messageRules"
+                    label="Message"
+                    
+                  ></v-textarea>
+                </v-flex>
+
+                <input type="submit" class="v-btn v-btn-large success" />
+
+              </v-layout>
+            </v-container>
+          </v-form>
+
           </v-container>
         </v-flex>
       </v-layout>
